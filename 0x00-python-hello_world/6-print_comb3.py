d5caac707c65b0d7b8037b287b7e168cecb12ce8
Weltopa <patrick.welton@yahoo.com>
2021-09-28 18:32:01 -0700

    chmod

--- 0x00-python-hello_world/6-print_comb3.py
+++ 0x00-python-hello_world/6-print_comb3.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-for i in range(0, 10):
-    x = i + 1
-    for x in range(x, 10):
-        print("{}{}".fomrat(i, x), end="")
-        if i == 8 and x == 9:
-            print()
-        else:
-            print(", ", end="")
+for a in range(0, 10):
+    for b in range(0, 10):
+        if a == 8 and b == 9:
+            print("89")
+        elif a < b:
+            print("{:d}{:d}, ".format(a, b), end="")
+    
