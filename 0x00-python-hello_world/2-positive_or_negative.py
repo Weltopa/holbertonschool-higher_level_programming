ec11b5aaa58a40721fdc94a6a8f8cdeb79c64c9a
Weltopa <patrick.welton@yahoo.com>
2021-09-28 17:08:21 -0700

    pon

---
+++ 0x00-python-hello_world/2-positive_or_negative.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10, 10)
+i = 0
+if number > i:
+    print(number, "is positve")
+if number == i:
+    print(number, "is zero")
+if number < i:
+    print(number, "is negative")
