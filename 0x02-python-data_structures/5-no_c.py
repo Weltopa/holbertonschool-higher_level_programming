b3b2799fc294570f59a07abc03e46cb243b8397b
Weltopa <patrick.welton@yahoo.com>
2021-09-30 18:09:48 -0700

    9

--- 0x02-python-data_structures/5-no_c.py
+++ 0x02-python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
     i = 0
-    my_list = list9my_string)
+    my_list = list(my_string)
     for ch in my_list:
         if ch == 'c' or ch == 'C':
             del my_list[i]
