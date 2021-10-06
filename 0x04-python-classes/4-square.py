bf9e22051380bc25c3b4f087903ec46f37046e06
Weltopa <patrick.welton@yahoo.com>
2021-10-06 12:22:47 -0700

    pep8

--- 0x04-python-classes/4-square.py
+++ 0x04-python-classes/4-square.py
@@ -8,6 +8,7 @@ class Square:
 
     def __init__(self, size=0):
         """ Initialization of Square object
+
         Args:
             size: the size of the square
         """
@@ -35,4 +36,4 @@ class Square:
             raise TypeError("size must be an integer")
         if value < 0:
             raise ValueError("size must be >= 0")
-        self.__size = value"
+        self.__size = value
