37ce589c4470af227b0591b29225df87ee9fbd9f
Weltopa <patrick.welton@yahoo.com>
2021-10-06 06:25:31 -0700

    edit

--- 0x04-python-classes/1-square.py
+++ 0x04-python-classes/1-square.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
+"""module of stuff that does stuff"""
+
+
 class Square:
     """ Square class
     """
 
-    def __init__(self, size):
+    def __init__(self, size=0):
         """ Initialization of a Square object
 
         Args:
