0975085493262da4dd4c48934c0caeb19c6eeefa
Weltopa <patrick.welton@yahoo.com>
2021-10-05 20:45:26 -0700

    help

---
+++ 0x04-python-classes/3-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Creates a square class"""
+
+
+class Square:
+    """Class that defines a square"""
+    def __init__(self, size=0):
+        '''sets size and checks for errors'''
+        if type(size) is not int:
+            raise TypeError("size must be integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        '''returns area of square'''
+        return self.__size ** 2
