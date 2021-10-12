a76635c16c4d82112bf2eb5ce610e97da445b6a6
Weltopa <patrick.welton@yahoo.com>
2021-10-11 19:20:06 -0700

    TextIndent

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Prints text with two new lines after each: ., ?, :"""
+
+
+def text_indentation(text):
+    """checks if string then replaces accordingly"""
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+    newtext = text.replace('?', '?\n\n')
+    newtext = text.replace('.', '.\n\n')
+    newtext = text.replace(':', ':\n\n')
+    print("\n".join([item.strip() for item in newtext.split("\n")]), end="")
