e16b36e75e71970a5ff267aabbeb5581337bd088
Weltopa <patrick.welton@yahoo.com>
2021-10-12 18:41:14 -0700

    newtext

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -7,6 +7,6 @@ def text_indentation(text):
     if type(text) is not str:
         raise TypeError("text must be a string")
     newtext = text.replace('?', '?\n\n')
-    newtext = text.replace('.', '.\n\n')
-    newtext = text.replace(':', ':\n\n')
+    newtext = newtext.replace('.', '.\n\n')
+    newtext = newtext.replace(':', ':\n\n')
     print("\n".join([item.strip() for item in newtext.split("\n")]), end="")
