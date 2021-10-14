db390853cb113be7198d46b571d75dd5c215d3a3
Weltopa <patrick.welton@yahoo.com>
2021-10-13 18:05:46 -0700

    edit

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Define BaseGeo Class"""
+
+"""DEfines Class"""
+class BaseGeometry:
+    """Function that raises accordingly"""
+    def area(self):
+        raise Exception("area() is notimplemented")
