9d66742b10a690f687e6697a9237a5adea2dd4bc
Weltopa <patrick.welton@yahoo.com>
2021-10-15 19:43:42 -0700

    edit

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -3,7 +3,7 @@
 import json
 
 
-def load_from_jsonfile(filename):
+def load_from_json_file(filename):
     """makes obj from json"""
     with open(filename, 'r') as fil:
         return json.loads(fil.read())
