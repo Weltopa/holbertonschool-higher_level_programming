9b0d0f5e4180956d8f9477a5b7104b58b7e72bd5
Weltopa <patrick.welton@yahoo.com>
2021-10-21 18:49:22 -0700

    edit

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -52,8 +52,8 @@ class Rectangle(Base):
         """setter for x"""
         if type(value) is not int:
             raise TypeError("x must be an integer")
-        if value <= 0:
-            raise ValueError("x must be > 0")
+        if value < 0:
+            raise ValueError("x must be >= 0")
         self.__x = value
 
     @property
