a87b88e76e2d9ed74f69604ebd770cfa390ecce2
Weltopa <patrick.welton@yahoo.com>
2021-10-22 07:41:05 -0700

    square class

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Square class file"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Actual Square class"""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """Instantiation meth for squares"""
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """String representation of square"""
+        strrep = "[Square] ({}) {}/{} - ".format(self.id, self.x, self.y)
+        strrep = += str(self.width)
+        return strrep
