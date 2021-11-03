bbf2b4b1454780e2adfbf677aadc26f4d695c8e4
Weltopa <patrick.welton@yahoo.com>
2021-11-03 15:45:31 -0700

    cities

---
+++ 0x01-SQL_more_queries/7-cities.sql
@@ -0,0 +1,4 @@
+-- Create database and use table in database
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities ( id INT NOT NULL AUTO_INCREMENT, state_id INT NOT NULL, PRIMARY KEY(id), FOREIGN KEY(state_id) REFERENCES states (id), name VARCHAR(256) NOT NULL );
