28d79d358be65ffe1e06131f6bb0a930fad277e6
Weltopa <patrick.welton@yahoo.com>
2021-11-05 18:14:18 -0700

    edit

--- 0x01-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x01-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,4 +1,4 @@
 -- Grouped by genre
-SELECT tv_genres.name genre, tv_show_genres.genre_id FROM tv_genres INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+SELECT tv_genres.name genre, COUNT(tv_show_genres.genre_id) number_of_shows FROM tv_genres INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 GROUP BY tv_genres.name, tv_show_genres.genre_id
-ORDER BY number_ofshows DESC;
+ORDER BY number_of_shows DESC;
