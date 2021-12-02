382ae1e399590ecc185800ec591c445f2075ed73
Weltopa <patrick.welton@yahoo.com>
2021-12-01 19:32:16 -0800

    par

--- 0x0C-python-network_1/3-error_code.py
+++ 0x0C-python-network_1/3-error_code.py
@@ -7,7 +7,7 @@ from urllib.error import HTTPError
 
 if __name__ == "__main__":
     try:
-        with urlopen(av[1] as response:
+        with urlopen(av[1]) as response:
                 print(response.read().decode("utf-8"))
 
     except HTTPError as err:
