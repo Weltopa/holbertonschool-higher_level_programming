a4502fc2cff696b2b0b5be38130b2adeb0c1e16f
Weltopa <patrick.welton@yahoo.com>
2021-12-01 19:30:26 -0800

    Error

---
+++ 0x0C-python-network_1/3-error_code.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Script that sends request to URL"""
+from sys import argv as av
+from urllib.request import urlopen
+from urllib.error import HTTPError
+
+
+if __name__ == "__main__":
+    try:
+        with urlopen(av[1] as response:
+                print(response.read().decode("utf-8"))
+
+    except HTTPError as err:
+        print("Error code: {}".format(err.code))
