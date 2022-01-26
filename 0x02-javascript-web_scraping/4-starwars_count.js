e9bbeb7f16df1fece59986a5bb95064dc0d5ecb4
Weltopa <patrick.welton@yahoo.com>
2022-01-26 07:38:32 -0800

    retry

--- 0x02-javascript-web_scraping/4-starwars_count.js
+++ 0x02-javascript-web_scraping/4-starwars_count.js
@@ -5,7 +5,14 @@ const url = process.argv[2];
 
 GET(url, (err, res, body) => {
   if (err) return (console.log(err));
-  const movies(filename, body, 'utf8', (err) => {
-    if (err) return (console.log(err));
-  });
+  const data = JSON.parse(body);
+  const tasks = {};
+
+  for (let x = 0; x < data.length; x++) {
+    if (data[x].completed) {
+      const id = data[x].userId;
+      tasks[id] !== undefined ? tasks[id] += 1 : tasks[id] = 1;
+    }
+  }
+  console.log(tasks);
 ]);
