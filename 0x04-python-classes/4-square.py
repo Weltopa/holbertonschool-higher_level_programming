07dda545ee68b663792a1af74658f68a51d65053
Weltopa <patrick.welton@yahoo.com>
2022-01-26 07:10:24 -0800

    retry

--- 0x04-python-classes/4-square.py
+++ 0x04-python-classes/4-square.py
@@ -3,28 +3,25 @@
 
 
 class Square:
-    """ Square class
-    """
-
+    """ Square class """
     def __init__(self, size=0):
         """ Initialization of Square object """
+        self.size = size
 
-    """Function for area"""
-    def area(self):
-        return self.size__size ** 2
+        @property
+        def size(self):
+            return self.__size
 
-    """Getter"""
-    @property
-    def size(self):
-        return self.__size
+        @size.setter
+        def size(self, size):
+            """Set size"""
+            if type(size) is not int:
+                raise TypeError("size must be an integer")
+            elif size < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = size
 
-    """Setter"""
-    @size.setter
-    def size(self, size):
-        """Set size"""
-        if type(value) is not int:
-            raise TypeError("size must be an integer")
-        elif value < 0:
-            raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
+        def area(self):
+            """ Returns area """
+            return self.__size ** 2
